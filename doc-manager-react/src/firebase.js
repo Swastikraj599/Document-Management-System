6188e1864917e9c48173ff979e3fe06fe0e3d2cd
Swastik raj <118122689+Swastikraj599@users.noreply.github.com>
2025-07-05 13:04:21 +0530

    Update firebase.js

--- doc-manager-react/src/firebase.js
+++ doc-manager-react/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1HpQF9FbWKsnZzYT8jpIoZXtTS8y72D8",
-  authDomain: "login-example-2ee43.firebaseapp.com",
-  projectId: "login-example-2ee43",
-  storageBucket: "login-example-2ee43.appspot.com",
-  messagingSenderId: "185903382168",
-  appId: "1:185903382168:web:5ae3e9ff7f4232b161daed"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
